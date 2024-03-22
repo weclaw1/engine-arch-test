13c9806658ad278a2a5d404e755e29e29176352e
Robert Węcławski <r.weclawski@gmail.com>
2024-03-22 14:40:13 +0100

    feat: add search

--- src/components/RolePanelViewList.vue
+++ src/components/RolePanelViewList.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
 const views = ref([
   { id: 1, name: 'Dispatcher Screen' },
@@ -7,6 +7,11 @@ const views = ref([
   { id: 3, name: 'Live Panel' },
   { id: 4, name: 'Client Communication Messages' },
 ]);
+
+const search = ref('');
+const filteredViews = computed(() => {
+  return views.value.filter((view) => view.name.toLowerCase().includes(search.value.toLowerCase()));
+});
 
 const selectedViews = ref<{ id: number; name: string }[]>([]);
 
@@ -19,9 +24,10 @@ const onChange = () => {
 };
 </script>
 <template>
+  <input type="text" v-model="search" placeholder="Search" />
   <p>Select views to add to that role</p>
   <ul>
-    <li v-for="view in views" :key="view.id">
+    <li v-for="view in filteredViews" :key="view.id">
       <input type="checkbox" v-model="selectedViews" :value="view" @change="onChange" />
       {{ view.name }}
     </li>
